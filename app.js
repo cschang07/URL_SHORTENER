edb4f29dfb6916b69d42914205e28835cc5a479c
cschang07 <manutdsfan@gmail.com>
2021-10-13 11:31:58 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const exphbs = require('express-handlebars');
 
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/url_shortener_1013', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
@@ -17,9 +17,14 @@ db.once('open', () => {
 })
 
 
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(3000, () => {
