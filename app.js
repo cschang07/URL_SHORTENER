1618702b34827d3898d191743e52787804fe3b54
cschang07 <manutdsfan@gmail.com>
2021-10-15 11:52:18 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -6,23 +6,7 @@ const Handlebars = require('handlebars')
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 
 const routes = require('./routes')
-const Url = require('./models/url')
-
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect('mongodb://localhost/url_shortener_1013', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', handlebars: allowInsecurePrototypeAccess(Handlebars) }))
 app.set('view engine', 'hbs')
